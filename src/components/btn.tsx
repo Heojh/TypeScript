6dc06836abb29862346e0e0d8604e37abff861b8
Heojh <jihaeng1989@gmail.com>
2022-04-21 13:04:37 +0900

    [Update] onPress 'input' type change (number -> union)

--- src/components/btn.tsx
+++ src/components/btn.tsx
@@ -8,10 +8,11 @@ function Btn() {
   const dispatch = useDispatch();
   const {count_data} = useSelector(state => state.reducer);
 
-  // git push
-  function onPress(input: number) {
+  // onPress 'input' type change (number -> union)
+  // -1, 1 이외에 다른 숫자를 넣었을 생길 수 있는 오류 방지
+  // string일 경우 오타로 인해 생길 수 있는 오류 방지
+  function onPress(input: -1 | 1) {
     let nextValue = (count_data + input) % 10;
-    console.log(nextValue);
     nextValue = nextValue < 0 ? 10 + nextValue : nextValue;
     dispatch(CountData(nextValue));
   }
